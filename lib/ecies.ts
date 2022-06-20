3885fe359cd6629dcb5a3e7e44b697b550a37242
Julia-a11y <695678541@qq.com>
2022-06-20 18:47:51 +0800

    ECIES modify

--- lib/ecies.ts
+++ lib/ecies.ts
@@ -2,130 +2,127 @@ import * as BN from 'bn.js'
 import * as cryptoJS from "crypto-js"
 import * as elliptic from 'elliptic'
 import {Rand, Prime} from "@safeheron/crypto-rand"
+import {Hex, CryptoJSBytes} from "@safeheron/crypto-utils"
+
 const P256 = elliptic.ec('p256')
-import {Hex, UrlBase64, CryptoJSBytes} from "@safeheron/crypto-utils"
 
 export namespace ECIES {
-
     /**
      * Encryption.
      * @param pub. ECC Public Key
      * @param plainBytes
-     * @returns {Promise<(*|BN|string|T[]|T[]|Buffer|WordArray)[]>}
+     * @param r The private key
+     * @param ivWordArray AES encrypt iv
+     * @returns {CryptoJSBytes}: [gR.encode()||aesCypher||macCypher||ivWordArray]
      * @private
      */
-    async function _encryptCryptoJSBytes(pub: any, plainBytes: CryptoJSBytes): Promise<[any, any, CryptoJSBytes, CryptoJSBytes, CryptoJSBytes]> {
-        const r = await Rand.randomBNLt(P256.n)
-        const ivBytes = await Rand.randomBytes(16)
-        const ivCryptoJSBytes = Hex.toCryptoJSBytes(Hex.fromBytes(ivBytes))
-        return _encryptCryptoJSBytesWithRIV(pub, plainBytes, r, ivCryptoJSBytes)
-    }
-
-
-    /**
-     * Encryption.
-     * @param pub. ECC Public Key
-     * @param plainBytes
-     * @param r
-     * @param iv
-     * @returns [any, any, CryptoJSBytes, CryptoJSBytes, CryptoJSBytes]: [gR.x, gR.y, iv, mac_cypher, cypher]
-     * @private
-     */
-    function _encryptCryptoJSBytesWithRIV(pub: any, plainBytes: CryptoJSBytes, r: BN, iv: CryptoJSBytes): [any, any, CryptoJSBytes, CryptoJSBytes, CryptoJSBytes] {
-        // Get Key
+    function _encryptCryptoJSBytesWithRIV(pub: any, plainBytes: CryptoJSBytes, r: BN, ivWordArray: CryptoJSBytes): CryptoJSBytes {
+        // get share Key
         let gR = P256.g.mul(r)
         let keyPoint = pub.mul(r)
 
         //generate seed
-        let keyPoint_x = keyPoint.getX()
-        let seed_WordArray = cryptoJS.lib.WordArray.create();
+        let seed = cryptoJS.lib.WordArray.create();
 
-        seed_WordArray.concat(cryptoJS.enc.Hex.parse(Hex.padLength(Hex.fromBytes(gR.encode()), 65 * 2)))
-        seed_WordArray.concat(cryptoJS.enc.Hex.parse(Hex.pad64(keyPoint_x.toString(16))))
+        let keyPointX = keyPoint.getX()
+        let gREncode = gR.encode()
 
-        let symm_key_size = 256 / 8
-        let mac_key_size = 512 / 8
-        let digestLength = 256 / 8
+        seed.concat(cryptoJS.enc.Hex.parse(Hex.padLength(Hex.fromBytes(gREncode), 65 * 2)))
+        seed.concat(cryptoJS.enc.Hex.parse(Hex.pad64(keyPointX.toString(16))))
 
-        let derivation_key = ''
-        let totalLength = symm_key_size + mac_key_size
-        for (let i = 1; i <= totalLength / digestLength; i++) {
-            let sha256 = cryptoJS.algo.SHA256.create();
-            let i_WordArray = cryptoJS.enc.Hex.parse(Hex.pad8(i.toString(16)))
-            sha256.update(seed_WordArray)
-            sha256.update(i_WordArray)
-            let digest = sha256.finalize();
-            derivation_key += Hex.pad64(digest.toString(cryptoJS.enc.Hex))
+        //generate derivation key
+        let symmKeyBytes = 256 / 8
+        let macKeyBytes = 1024 / 8
+        let digestBytes = 512 / 8
+
+        let derivationKey = ''
+        let totalBytes = symmKeyBytes + macKeyBytes
+        for (let i = 1; i <= (totalBytes +  digestBytes - 1)/ digestBytes; i++) {
+            let sha512 = cryptoJS.algo.SHA512.create();
+            let iWordArray = cryptoJS.enc.Hex.parse(Hex.pad8(i.toString(16)))
+            sha512.update(seed)
+            sha512.update(iWordArray)
+            let digest = sha512.finalize();
+            derivationKey += Hex.padLength(digest.toString(cryptoJS.enc.Hex), 128)
         }
+        derivationKey = derivationKey.substring(0, totalBytes*2)
 
-        let symm_key = derivation_key.substring(0, symm_key_size * 2)
-        let mac_key = derivation_key.substring(symm_key_size * 2, derivation_key.length)
+        let symmKey = derivationKey.substring(0, symmKeyBytes * 2)
+        let macKey = derivationKey.substring(symmKeyBytes * 2, derivationKey.length)
 
-        let iv_bytes = iv
-
-        // AES256, CBC default
-        let aesEncryptor = cryptoJS.algo.AES.createEncryptor(cryptoJS.enc.Hex.parse(symm_key), {iv: iv_bytes})
+        // encrypt, AES256, CBC default
+        let aesEncryptor = cryptoJS.algo.AES.createEncryptor(cryptoJS.enc.Hex.parse(symmKey), {iv: ivWordArray})
         let cypher1 = aesEncryptor.process(plainBytes)
         let cypher2 = aesEncryptor.finalize()
-        let cypher = cypher1.concat(cypher2)
+        let aesCypher = cypher1.concat(cypher2)
 
-        let hmac_sha256 = cryptoJS.algo.HMAC.create(cryptoJS.algo.SHA256, cryptoJS.enc.Hex.parse(mac_key));
-        hmac_sha256.update(cypher);
-        let mac_cypher = hmac_sha256.finalize();
+        //cal mac for cypher
+        let macIVLen = "0"
+        let hmac_sha512 = cryptoJS.algo.HMAC.create(cryptoJS.algo.SHA512, cryptoJS.enc.Hex.parse(macKey));
+        hmac_sha512.update(aesCypher);
+        hmac_sha512.update(cryptoJS.enc.Hex.parse(Hex.pad16(macIVLen)))
+        let macCypher = hmac_sha512.finalize();
 
-        return [gR.getX(), gR.getY(), iv_bytes, mac_cypher, cypher]
+        let gREncodeWordArray = cryptoJS.enc.Hex.parse(Hex.padLength(Hex.fromBytes(gREncode), 65 * 2));
+
+        return gREncodeWordArray.concat(aesCypher).concat(macCypher).concat(ivWordArray)
     }
 
     /**
      * Decryption
      * @param gR. Curve.g^R
      * @param priv.
-     * @param iv_bytes
+     * @param ivWordArray AES encrypt iv
+     * @param aesCypher
      * @param macCypher
-     * @param cypherPart
-     * @returns CryptoJSBytes
+     * @returns {CryptoJSBytes} PLain
      * @private
      */
-    function _decryptCryptoJSBytes(gR: any, priv: BN, iv_bytes: CryptoJSBytes, macCypher: CryptoJSBytes, cypherPart: CryptoJSBytes): CryptoJSBytes {
-        // Get key
+    function _decryptCryptoJSBytes(gR: any, priv: BN, ivWordArray: CryptoJSBytes, aesCypher: CryptoJSBytes, macCypher: CryptoJSBytes): CryptoJSBytes {
+        // get share key
         let keyPoint = gR.mul(priv)
 
         //generate seed
-        let keyPoint_x = keyPoint.getX()
-        let seed_WordArray = cryptoJS.lib.WordArray.create();
-        seed_WordArray.concat(cryptoJS.enc.Hex.parse(Hex.padLength(Hex.fromBytes(gR.encode()), 65 * 2)))
-        seed_WordArray.concat(cryptoJS.enc.Hex.parse(Hex.pad64(keyPoint_x.toString(16))))
+        let keyPointX = keyPoint.getX()
+        let gREncode = gR.encode()
 
-        let symm_key_size = 256 / 8
-        let mac_key_size = 512 / 8
-        let digestLength = 256 / 8
+        let seed = cryptoJS.lib.WordArray.create()
+        seed.concat(cryptoJS.enc.Hex.parse(Hex.padLength(Hex.fromBytes(gREncode), 65 * 2)))
+        seed.concat(cryptoJS.enc.Hex.parse(Hex.pad64(keyPointX.toString(16))))
 
-        let derivation_key = ''
-        let totalLength = symm_key_size + mac_key_size
-        for (let i = 1; i <= totalLength / digestLength; i++) {
-            let sha256 = cryptoJS.algo.SHA256.create();
-            let i_WordArray = cryptoJS.enc.Hex.parse(Hex.pad8(i.toString(16)))
-            sha256.update(seed_WordArray)
-            sha256.update(i_WordArray)
-            let digest = sha256.finalize();
-            derivation_key += Hex.pad64(digest.toString(cryptoJS.enc.Hex))
+        //generate derivation key
+        let symmKeyBytes = 256 / 8
+        let macKeyBytes = 1024 / 8
+        let digestBytes = 512 / 8
+
+        let derivationKey = ''
+        let totalBytes = symmKeyBytes + macKeyBytes
+        for (let i = 1; i <= (totalBytes + digestBytes - 1)/ digestBytes; i++) {
+            let sha512 = cryptoJS.algo.SHA512.create();
+            let iWordArray = cryptoJS.enc.Hex.parse(Hex.pad8(i.toString(16)))
+            sha512.update(seed)
+            sha512.update(iWordArray)
+            let digest = sha512.finalize();
+            derivationKey += Hex.padLength(digest.toString(cryptoJS.enc.Hex), 128)
         }
+        derivationKey = derivationKey.substring(0, totalBytes*2)
 
-        let symm_key = derivation_key.substring(0, symm_key_size * 2)
-        let mac_key = derivation_key.substring(symm_key_size * 2, derivation_key.length)
+        let symmKey = derivationKey.substring(0, symmKeyBytes * 2)
+        let macKey = derivationKey.substring(symmKeyBytes * 2, derivationKey.length)
 
         //cal mac for cypher
-        let hmac_sha256 = cryptoJS.algo.HMAC.create(cryptoJS.algo.SHA256, cryptoJS.enc.Hex.parse(mac_key));
-        hmac_sha256.update(cypherPart);
-        let mac_cypher = hmac_sha256.finalize();
-
-        if (mac_cypher.toString(cryptoJS.enc.Hex) != macCypher.toString(cryptoJS.enc.Hex)) {
-            throw 'Mac verify error: mac_cypher != macCypher'
+        let macIVLen = "0"
+        let hmac_sha512 = cryptoJS.algo.HMAC.create(cryptoJS.algo.SHA512, cryptoJS.enc.Hex.parse(macKey));
+        hmac_sha512.update(aesCypher);
+        hmac_sha512.update(cryptoJS.enc.Hex.parse(Hex.pad16(macIVLen)))
+        let verifyMacCypher = hmac_sha512.finalize();
+        if (verifyMacCypher.toString(cryptoJS.enc.Hex) != macCypher.toString(cryptoJS.enc.Hex)) {
+            throw 'Mac verify error: verifyMacCypher != macCypher'
         }
 
-        // Decrypt
-        let aesDecryptor = cryptoJS.algo.AES.createDecryptor(cryptoJS.enc.Hex.parse(symm_key), {iv: iv_bytes});
-        let plainPart1 = aesDecryptor.process(cypherPart);
+        // decrypt
+        let aesDecryptor = cryptoJS.algo.AES.createDecryptor(cryptoJS.enc.Hex.parse(symmKey), {iv: ivWordArray});
+        let plainPart1 = aesDecryptor.process(aesCypher);
         let plainPart2 = aesDecryptor.finalize();
         return plainPart1.concat(plainPart2)
     }
@@ -133,56 +130,63 @@ export namespace ECIES {
     /**
      * Encrypt CryptoJSBytes to cypher CryptoJSBytes.
      * @param pub. ECC Public Key
-     * @param plainBytes.
+     * @param plainCryptoJSBytes.
      * @returns {Promise<CryptoJSBytes>}
      */
-    export async function encryptCryptoJSBytes(pub:any, plainBytes:  CryptoJSBytes): Promise<CryptoJSBytes>{
-        let [x_gR, y_gR, iv_bytes, macCypher, cypherPart] = await _encryptCryptoJSBytes(pub, plainBytes)
-        let xBytes = cryptoJS.enc.Hex.parse(Hex.pad64(x_gR.toString(16)))
-        let yBytes = cryptoJS.enc.Hex.parse(Hex.pad64(y_gR.toString(16)))
-        return xBytes.concat(yBytes).concat(iv_bytes).concat(macCypher).concat(cypherPart)
+    export async function encryptCryptoJSBytes(pub: any, plainCryptoJSBytes:  CryptoJSBytes): Promise<CryptoJSBytes> {
+        const r = await Rand.randomBNLt(P256.n)
+        let iv = await Rand.randomBytes(16)
+        const ivCryptoJSBytes = Hex.toCryptoJSBytes(Hex.fromBytes(iv))
+        return await _encryptCryptoJSBytesWithRIV(pub, plainCryptoJSBytes, r, ivCryptoJSBytes)
+
     }
 
     /**
      * Encrypt CryptoJSBytes to cypher CryptoJSBytes with specified random IV.
      * @param pub. ECC Public Key
-     * @param plainBytes.
+     * @param plainCryptoJSBytes.
      * @param r. BN
      * @param iv. BN
      * @returns {Promise<CryptoJSBytes>}
      */
-    export async function encryptCryptoJSBytesWithRIV(pub: any, plainBytes: CryptoJSBytes, r: BN, iv: CryptoJSBytes): Promise<CryptoJSBytes> {
-        let [x_gR, y_gR, iv_bytes, macCypher, cypherPart] = await _encryptCryptoJSBytesWithRIV(pub, plainBytes, r, iv)
-        let xBytes = cryptoJS.enc.Hex.parse(Hex.pad64(x_gR.toString(16)))
-        let yBytes = cryptoJS.enc.Hex.parse(Hex.pad64(y_gR.toString(16)))
-        return xBytes.concat(yBytes).concat(iv_bytes).concat(macCypher).concat(cypherPart)
+    export async function encryptCryptoJSBytesWithRIV(pub: any, plainCryptoJSBytes: CryptoJSBytes, r: BN, iv: any[]): Promise<CryptoJSBytes> {
+        const ivCryptoJSBytes = Hex.toCryptoJSBytes(Hex.fromBytes(iv))
+        //const ivCryptoJSBytes = Hex.toCryptoJSBytes(iv)
+        return await _encryptCryptoJSBytesWithRIV(pub, plainCryptoJSBytes, r, ivCryptoJSBytes)
     }
 
     /**
      * Decrypt CryptoJSBytes to plain CryptoBytes.
      * @param priv. Private Key.
-     * @param cypherBytes
-     * @returns {CryptoJSBytes}
+     * @param cypherCryptoJSBytes
+     * @returns {CryptoJSBytes} Plain
      */
-    export function decryptCryptoJSBytes(priv: BN, cypherBytes: CryptoJSBytes): CryptoJSBytes {
+    export function decryptCryptoJSBytes(priv: BN, cypherCryptoJSBytes: CryptoJSBytes): CryptoJSBytes {
         // Split cypher data
-        let cypherStr = cryptoJS.enc.Hex.stringify(cypherBytes)
+
+        let cypherStr = cryptoJS.enc.Hex.stringify(cypherCryptoJSBytes)
         let start = 0
+        let prefix = cypherStr.substring(start, start + 2)
+        if(prefix != "04") {
+            throw 'prefix error'
+        }
+        start += 2
         let gR_x = cypherStr.substring(start, start + 64)
         start += 64
         let gR_y = cypherStr.substring(start, start + 64)
         start += 64
-        let iv = cypherStr.substring(start, start + 32)
-        start += 32
-        let macCypher = cypherStr.substring(start, start + 64)
-        start += 64
-        let cypherPart = cypherStr.substring(start)
-        // Get Key and IV
+
+        let aesCypher = cypherStr.substring(start, cypherStr.length - 128 - 32)
+        let macCypher = cypherStr.substring(cypherStr.length - 128 - 32, cypherStr.length - 32)
+        let iv = cypherStr.substring(cypherStr.length - 32, cypherStr.length)
+
+
         let gR = P256.curve.point(gR_x, gR_y)
-        let iv_bytes = cryptoJS.enc.Hex.parse(iv)
+        let ivWordArray = cryptoJS.enc.Hex.parse(iv)
+
+        aesCypher = cryptoJS.enc.Hex.parse(aesCypher)
         macCypher = cryptoJS.enc.Hex.parse(macCypher)
-        cypherPart = cryptoJS.enc.Hex.parse(cypherPart)
-        return _decryptCryptoJSBytes(gR, priv, iv_bytes, macCypher, cypherPart)
+        return _decryptCryptoJSBytes(gR, priv, ivWordArray, aesCypher, macCypher)
     }
 
     /**
@@ -191,9 +195,9 @@ export namespace ECIES {
      * @param plainBytes.
      * @returns {Promise<number>} cypher bytes.
      */
-    export async function encryptBytes(pub:any, plainBytes:  number[]): Promise<number[]>{
-        plainBytes = Hex.toCryptoJSBytes(Hex.fromBytes(plainBytes))
-        let cypherCryptoJSBytes = await encryptCryptoJSBytes(pub, plainBytes)
+    export async function encryptBytes(pub: any, plainBytes: number[]): Promise<number[]>{
+        let plainCryptoJSBytes = Hex.toCryptoJSBytes(Hex.fromBytes(plainBytes))
+        let cypherCryptoJSBytes = await encryptCryptoJSBytes(pub, plainCryptoJSBytes)
         return Hex.toBytes(Hex.fromCryptoJSBytes(cypherCryptoJSBytes))
     }
 
@@ -204,8 +208,8 @@ export namespace ECIES {
      * @returns {number[]}
      */
     export function decryptBytes(priv: BN, cypherBytes: number[]): number[] {
-        cypherBytes = Hex.toCryptoJSBytes(Hex.fromBytes(cypherBytes))
-        let plainCryptoJSBytes = decryptCryptoJSBytes(priv, cypherBytes)
+        let cypherCryptoJSBytes = Hex.toCryptoJSBytes(Hex.fromBytes(cypherBytes))
+        let plainCryptoJSBytes = decryptCryptoJSBytes(priv, cypherCryptoJSBytes)
         return Hex.toBytes(Hex.fromCryptoJSBytes(plainCryptoJSBytes))
     }
 
@@ -216,7 +220,8 @@ export namespace ECIES {
      * @returns {Promise<number[]>}
      */
     export async function encryptString(pub:any, plainStr: string): Promise<number[]>{
-        let cypherCryptoJSBytes = await encryptCryptoJSBytes(pub, plainStr)
+        let  plainCryptoJSBytes = cryptoJS.enc.Utf8.parse(plainStr)
+        let cypherCryptoJSBytes = await encryptCryptoJSBytes(pub, plainCryptoJSBytes)
         return Hex.toBytes(Hex.fromCryptoJSBytes(cypherCryptoJSBytes))
     }
 
@@ -227,8 +232,8 @@ export namespace ECIES {
      * @returns {string} plain
      */
     export function decryptString(priv: BN, cypherBytes: number[]): string {
-        cypherBytes = Hex.toCryptoJSBytes(Hex.fromBytes(cypherBytes))
-        let plainCryptoJSBytes = decryptCryptoJSBytes(priv, cypherBytes)
+        let cypherCryptoJSBytes = Hex.toCryptoJSBytes(Hex.fromBytes(cypherBytes))
+        let plainCryptoJSBytes = decryptCryptoJSBytes(priv, cypherCryptoJSBytes)
         return cryptoJS.enc.Utf8.stringify(plainCryptoJSBytes)
     }
 }
